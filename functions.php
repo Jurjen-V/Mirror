e37236c06bb965a867b8a74cd071e098e147b140
sandeeeerr <sandervries@me.com>
2019-03-19 13:20:31 +0100

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function actionButtons() {
+    ?>
+    <div class="actions">
+        <a href="#"><i class="fas fa-trash-alt"></i></a>
+        <a href="#"><i class="fas fa-edit"></i></a>
+    </div>
+    <?php
+}
+?>
---
+++ login.php
@@ -0,0 +1,24 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin</title>
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+
+    <img class="logo" src="https://seeklogovector.net/wp-content/uploads/2018/10/roc-friese-poort-logo-vector.png" alt="">
+    <form method="post" action="login.php">
+      	<?php include('errors.php'); ?>
+        <label>Username</label>
+        <input type="text" name="username" >
+        <label>Password</label>
+        <input type="password" name="password">
+      	<button type="submit" class="btn" name="login_user">Login</button>
+    </form>
+
+</body>
+</html>
