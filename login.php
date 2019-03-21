b0b490bc17581bbf0b084b76f7e096fc1b7ff695
sandeeeerr <sandervries@me.com>
2019-03-21 09:15:45 +0100

    Update login.php

--- login.php
+++ login.php
@@ -6,19 +6,24 @@
     <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<!-- <link rel="stylesheet" type="text/css" href="css/login.css"> -->
+	<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 <body>
 
-    <img class="logo" src="https://seeklogovector.net/wp-content/uploads/2018/10/roc-friese-poort-logo-vector.png" alt="">
-    <form method="post" action="login.php">
-      	<?php include('errors.php'); ?>
-        <label>Username</label>
-        <input type="text" name="username" >
-        <label>Password</label>
-        <input type="password" name="password">
-      	<button type="submit" class="btn" name="login_user">Login</button>
-    </form>
+    <header>
+        <img class="logo" src="https://i.imgur.com/05a8QY5.png" alt="">
+    </header>
+    <main>
+        <form method="post" action="login.php">
+            <?php include('errors.php'); ?>
+            <h3>Hall Of Fame Login</h3>
+            <input type="text" name="username" placeholder="&#61447;">
+            <br>
+            <input type="password" name="password" placeholder="&#xf084;">
+            <br>
+            <button type="submit" class="btn" name="login_user">Login</button>
+        </form>
+    </main>
 
 </body>
 </html>
