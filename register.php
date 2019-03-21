5d8ccf28651beb6b540b7cef695bff27e4e276e2
sandeeeerr <sandervries@me.com>
2019-03-21 09:21:59 +0100

    Developer check ingebouwd

--- register.php
+++ register.php
@@ -1,16 +1,4 @@
-<?php
-  session_start();
 
-  if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: login.php");
-  }
-?>
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
@@ -19,34 +7,36 @@
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <div class="header">
-  	<h2>Register</h2>
-  </div>
+  <?php
+    if (isset($_SESSION['username']) && $_SESSION['login_user'] == 1553085554) {
+    ?>
+        <form method="post" action="register.php">
+          <?php include('errors.php'); ?>
+          <div class="input-group">
+            <label>Gebruikersnaam</label>
+            <input type="text" name="username" value="<?php echo $username; ?>">
+          </div>
+          <div class="input-group">
+            <label>Email</label>
+            <input type="email" name="email" value="<?php echo $email; ?>">
+          </div>
+          <div class="input-group">
+            <label>Wachtwoord</label>
+            <input type="password" name="password_1">
+          </div>
+          <div class="input-group">
+            <label>Bevestig Wachtwoord</label>
+            <input type="password" name="password_2">
+          </div>
+          <div class="input-group">
+            <button type="submit" class="btn" name="reg_user">Registreer</button>
+          </div>
+        </form>
+    <?php
+    } else {
+        echo "Je hebt geen toegang tot deze pagina";
+    }
+  ?>
 
-  <form method="post" action="register.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Email</label>
-  	  <input type="email" name="email" value="<?php echo $email; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Password</label>
-  	  <input type="password" name="password_1">
-  	</div>
-  	<div class="input-group">
-  	  <label>Confirm password</label>
-  	  <input type="password" name="password_2">
-  	</div>
-  	<div class="input-group">
-  	  <button type="submit" class="btn" name="reg_user">Register</button>
-  	</div>
-  	<p>
-  		Already a member? <a href="login.php">Sign in</a>
-  	</p>
-  </form>
 </body>
 </html>
